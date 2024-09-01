785243aa4ffff21cb88e92279d77bee83d3875bc
Alhena Minhaz <alhena.minhazz@gmail.com>
2024-09-01 21:57:10 +0530

    adding module changes

---
+++ src/app/components/match-list/match-list.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatchListComponent } from './match-list.component';
+
+@NgModule({
+  declarations: [
+    MatchListComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MatchListComponent
+  ]
+})
+export class MatchListModule { }
---
+++ src/app/components/participant-list/participant-list.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ParticipantListComponent } from './participant-list.component';
+
+@NgModule({
+  declarations: [
+    ParticipantListComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ParticipantListComponent
+  ]
+})
+export class ParticipantListModule { }
